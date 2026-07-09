883d35a68d3e69144d390b9e36a28de5da3a8885
Julieta Bertorello Sartori <julietabertorello@gmail.com>
2026-07-09 18:25:25 +0200

    refactor: replace native img tags with next/image in Hero component and update animation classes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Hero({ data }: { data?: any }) {
   const overhead = data?.overhead || "Una infancia acompañada puede <b>cambiarlo todo</b>";
@@ -10,16 +11,13 @@ export default function Hero({ data }: { data?: any }) {
 
       <div className="wrap hero__inner">
         <div className="hero__copy">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
+          <Image
             src="https://res.cloudinary.com/djqtkbyez/image/upload/f_auto,q_auto/v1780667034/LOGOSVOLVERACASA_Mesa_de_trabajo_1_eh3gyo.png"
             alt="Logo Volver a Casa"
             className="hero-mobile-logo"
             width={240}
             height={80}
-            fetchPriority="high"
-            loading="eager"
-            decoding="async"
+            priority
           />
           <p className="hero__kicker hablemos" data-reveal="" dangerouslySetInnerHTML={{ __html: overhead }} />
           <h1 data-reveal="" data-delay="1" dangerouslySetInnerHTML={{ __html: titleHtml }} />
@@ -29,35 +27,29 @@ export default function Hero({ data }: { data?: any }) {
       {/* erizos for variant B */}
       <div className="hero-bandstage" aria-hidden="true">
         <div
-          className="erizo e2 float-slow band-erizo"
+          className="erizo e2 float band-erizo"
           style={{ "--rot": "5deg" } as React.CSSProperties}
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
+          <Image
             src="https://res.cloudinary.com/djqtkbyez/image/upload/v1780667171/ELEMENTOSGRA%CC%81FICOSI-05_avyp6m.png"
             alt="Ilustración de erizo que representa la emoción de la ansiedad"
             style={{ transform: "scaleX(-1)" }}
             width={150}
             height={150}
-            fetchPriority="high"
-            loading="eager"
-            decoding="async"
+            loading="lazy"
           />
           <span className="erizo-bubble">Hola, soy ansiedad</span>
         </div>
         <div
-          className="erizo e3 float-slow band-erizo"
+          className="erizo e3 float band-erizo"
           style={{ "--rot": "3deg" } as React.CSSProperties}
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
+          <Image
             src="https://res.cloudinary.com/djqtkbyez/image/upload/v1780667171/ELEMENTOSGRA%CC%81FICOSI_Mesa_de_trabajo_1_gp9noj.png"
             alt="Ilustración de erizo que representa la emoción de la alegría"
             width={150}
             height={150}
-            fetchPriority="high"
-            loading="eager"
-            decoding="async"
+            loading="lazy"
           />
           <span className="erizo-bubble">Hola, soy alegría</span>
         </div>
@@ -65,14 +57,12 @@ export default function Hero({ data }: { data?: any }) {
           className="erizo e4 float band-erizo"
           style={{ "--rot": "-5deg" } as React.CSSProperties}
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
+          <Image
             src="https://res.cloudinary.com/djqtkbyez/image/upload/v1780690160/ELEMENTOSGRA%CC%81FICOSI-04_zvviok.png"
             alt="Ilustración de erizo que representa la emoción del miedo"
-            style={{ transform: "scaleX(-1)" }}
-            fetchPriority="high"
-            loading="eager"
-            decoding="async"
+            width={150}
+            height={150}
+            loading="lazy"
           />
           <span className="erizo-bubble">Hola, soy miedo</span>
         </div>
